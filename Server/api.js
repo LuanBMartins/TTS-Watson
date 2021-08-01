2e788616147a0a92474afde9bf36bdf6d4635b02
LuanBMartins <luan.cbm@hotmail.com>
2021-07-31 21:47:02 -0300

    add cors config

--- Server/api.js
+++ Server/api.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const textRoute = require('./routes/textRoute')
 const PORT = 3000
 
 
+app.use(cors())
 app.use(express.json())
 app.use('/', textRoute)
 
