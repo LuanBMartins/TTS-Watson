f2f192f90752175fc43d24b00ed4ec0371e54dfb
LuanBMartins <luan.cbm@hotmail.com>
2021-08-01 21:51:06 -0300

    add comments/interface

--- client/src/page/tts.js
+++ client/src/page/tts.js
@@ -5,6 +5,12 @@ import axios from 'axios'
 const Tts = () => {
     const [comment, setComment] = useState([])
     const [text, setText] = useState('')
+    const [num, setAtt] = useState(0)
+
+    useEffect(() => {
+        request('get', 'http://localhost:3000/api/audios')
+            .then(response => setComment(response.data))
+    }, [num])
 
     const request = (method, url, data) => {
         return axios({ method, url, data, validateStatus: false })
@@ -14,10 +20,21 @@ const Tts = () => {
         setText(event.target.value)
     }
 
+    const audioPlay = async (event) => {
+        const [message] = comment.filter(item => item.id === Number(event.target.id))
+        let audio = new Audio(`http://localhost:3000/api/audios/${message.id}`)
+        audio.play()
+    }
+
+    const handleClickDelete = async (event) => {
+        const [message] = comment.filter(item => item.id === Number(event.target.id))
+        await request('delete',`http://localhost:3000/api/audios/${message.id}`)
+        setAtt(oldNum => oldNum + 1)
+    }
+
     const handleClick = async () => {
         if (text === '') return false
         const response = await request('post', 'http://localhost:3000/api/audios', {text: text})
-        console.log('log',response);
         setComment(oldArray => [...oldArray, response.data])
         setText('')
     }
@@ -29,6 +46,16 @@ const Tts = () => {
                 <textarea id="text" placeholder="Digite aqui" value={text} onChange={handleChange} /><br></br>
                 <button type="button" onClick={handleClick}>Gerar audio</button>
             </form>
+
+            <div>
+               {comment.map(item => {
+                   return <li key={item.id}>
+                       {item.message} 
+                       <button id={item.id} onClick={handleClickDelete}>Deletar</button>
+                       <button id={item.id} onClick={audioPlay}>Tocar</button>
+                   </li>
+               })}
+            </div>
         </div>
     )
 }
